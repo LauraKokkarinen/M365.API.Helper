e8165cec6ebf9dd0f08f40788caf5e3cdfee2963
Laura Kokkarinen <Laura.Kokkarinen@sulava.com>
2025-01-23 12:38:47 +0200

    Updated interfaces.

--- M365.API.Helper/Interfaces/IGraphService.cs
+++ M365.API.Helper/Interfaces/IGraphService.cs
@@ -2,6 +2,11 @@
 {
     public interface IGraphService
     {
-        
+        Task<T?> Get<T>(string url);
+        Task<IEnumerable<T>> GetCollection<T>(string url, IEnumerable<T>? items = null);
+        Task<T?> Post<T>(string url, string? body = null);
+        Task<T?> Patch<T>(string url, string body);
+        Task<T?> Put<T>(string url, string body);
+        Task Delete(string url);
     }
 }
--- M365.API.Helper/Interfaces/ISharePointService.cs
+++ M365.API.Helper/Interfaces/ISharePointService.cs
@@ -2,6 +2,11 @@
 {
     public interface ISharePointService
     {
-
+        Task<T?> Get<T>(string url, string? contentType = null, string? acceptLanguage = null);
+        Task<IEnumerable<T>> GetCollection<T>(string url, string? contentType = null, string? acceptLanguage = null, IEnumerable<T>? items = null);
+        Task<T?> Post<T>(string url, string? body = null, string? contentType = null, string? acceptLanguage = null);
+        Task<T?> Patch<T>(string url, string body, string? contentType = null, string? acceptLanguage = null);
+        Task<T?> Put<T>(string url, string body, string? contentType = null, string? acceptLanguage = null);
+        Task Delete(string url);
     }
 }
